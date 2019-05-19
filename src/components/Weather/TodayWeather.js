61a0c135e54149c2711822e76ef7919151bbc277
Lain Yeo <lainfox@gmail.com>
2019-05-19 15:34:35 +0900

    Fetch right city weather api

--- src/components/Weather/TodayWeather.js
+++ src/components/Weather/TodayWeather.js
@@ -1,26 +1,36 @@
 import React from 'react';
 
-const API_WEATHER = 'http://demo6468405.mockable.io/weather-crawlers/current-weathers/by-city-name/Daejeon';
+const API_WEATHER = 'http://demo6468405.mockable.io/weather-crawlers/current-weathers/by-city-name';
 
 class TodayWeather extends React.Component {
   state = {
-    weather: {}
+    weather: null
   };
 
   async componentDidMount() {
-    // const weather = await fetch(API_WEATHER)
-    //   .then(res => res.json())
-    //   .then(data => data);
-    // this.setState({
-    //   weather
-    // });
+    // const { cityId } = this.props.match.params;
+    const cityId = 'Daejeon';
+    const api = `${API_WEATHER}/${cityId}`;
+
+    const weather = await fetch(api)
+      .then(res => res.json())
+      .then(data => data);
+
+    this.setState({
+      weather
+    });
   }
 
   render() {
     const { cityId } = this.props.match.params;
     const { weather } = this.state;
 
-    return <div>{cityId}</div>;
+    return (
+      <>
+        <h2>{cityId}</h2>
+        {weather && <div>{JSON.stringify(weather)}</div>}
+      </>
+    );
   }
 }
 
